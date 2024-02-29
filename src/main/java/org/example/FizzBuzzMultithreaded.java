d380af9b7557b85f63bbe5e510ad174c71e43017
Світлана <simbratovich@gmail.com>
2024-02-29 23:48:45 +0200

    Доопрацьовано

--- src/main/java/org/example/FizzBuzzMultithreaded.java
+++ src/main/java/org/example/FizzBuzzMultithreaded.java
@@ -1,57 +1,96 @@
 package org.example;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class FizzBuzzMultithreaded {
     private int n;
-    private BlockingQueue<String> queueA = new LinkedBlockingQueue<>();
-    private BlockingQueue<String> queueB = new LinkedBlockingQueue<>();
-    private BlockingQueue<String> queueC = new LinkedBlockingQueue<>();
-    private BlockingQueue<String> queueD = new LinkedBlockingQueue<>();
+    private int current = 1;
+    private final Object lock = new Object();
 
     public FizzBuzzMultithreaded(int n) {
         this.n = n;
     }
 
-    public void fizz() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 != 0) {
-                queueD.put("fizz");
-            } else {
-                queueD.put(String.valueOf(i));
+    public void fizzbuzz() {
+        new Thread(() -> {
+            try {
+                for (int i = 1; i <= n; i++) {
+                    synchronized (lock) {
+                        while (current <= n && (current % 3 != 0 || current % 5 != 0)) {
+                            lock.wait();
+                        }
+                        if (current <= n) {
+                            System.out.println("fizzbuzz");
+                            current++;
+                            lock.notifyAll();
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }
+        }).start();
     }
 
-    public void buzz() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 == 0) {
-                queueD.put("buzz");
-            } else {
-                queueD.put(String.valueOf(i));
+    public void fizz() {
+        new Thread(() -> {
+            try {
+                for (int i = 1; i <= n; i++) {
+                    synchronized (lock) {
+                        while (current <= n && (current % 3 != 0 || current % 5 == 0)) {
+                            lock.wait();
+                        }
+                        if (current <= n) {
+                            System.out.println("fizz");
+                            current++;
+                            lock.notifyAll();
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }
+        }).start();
     }
 
-    public void fizzbuzz() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                queueD.put("fizzbuzz");
-            } else {
-                queueD.put(String.valueOf(i));
+    public void buzz() {
+        new Thread(() -> {
+            try {
+                for (int i = 1; i <= n; i++) {
+                    synchronized (lock) {
+                        while (current <= n && (current % 5 != 0 || current % 3 == 0)) {
+                            lock.wait();
+                        }
+                        if (current <= n) {
+                            System.out.println("buzz");
+                            current++;
+                            lock.notifyAll();
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }
+        }).start();
     }
 
-    public void number() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                queueD.put(String.valueOf(i));
+    public void number() {
+        new Thread(() -> {
+            try {
+                for (int i = 1; i <= n; i++) {
+                    synchronized (lock) {
+                        while (current <= n && (current % 3 == 0 || current % 5 == 0)) {
+                            lock.wait();
+                        }
+                        if (current <= n) {
+                            System.out.println(current);
+                            current++;
+                            lock.notifyAll();
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }
-    }
-    public String takeFromQueueD() throws InterruptedException {
-        return queueD.take();
+        }).start();
     }
 }
